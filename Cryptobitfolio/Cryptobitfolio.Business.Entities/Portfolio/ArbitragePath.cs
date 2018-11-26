3cf716652a4e4bde66f89f738352b0d241ac8a8f
Matt Scheetz <mfscheetz@gmail.com>
2018-11-26 14:15:12 -0500

    new object

---
+++ Cryptobitfolio/Cryptobitfolio.Business.Entities/Portfolio/ArbitragePath.cs
@@ -0,0 +1,28 @@
+﻿// -----------------------------------------------------------------------------
+// <copyright file="ArbitragePath" company="Matt Scheetz">
+//     Copyright (c) Matt Scheetz All Rights Reserved
+// </copyright>
+// <author name="Matt Scheetz" date="11/26/2018 1:46:43 PM" />
+// -----------------------------------------------------------------------------
+
+namespace Cryptobitfolio.Business.Entities.Portfolio
+{
+    #region Usings
+
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    #endregion Usings
+
+    public class ArbitragePath
+    {
+        #region Properties
+
+        public int Id { get; set; }
+        public DateTime Created { get; set; }
+        public string[] Path { get; set; }
+
+        #endregion Properties
+    }
+}
